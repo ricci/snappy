82af4d814aaa860fbc87b0b1477f2f19be556e09
Robert Ricci <ricci@cs.utah.edu>
2016-01-01 00:14:11 -0700

    Nice structure for storing commands
    Now, all commands have a structure that makes it easy to find them,
    print usage messages, etc.

--- snappylib/commands.py
+++ snappylib/commands.py
@@ -8,6 +8,47 @@ from snappylib.place import Place
 import snappylib.zfs as zfs
 import snappylib.tarsnap as tarsnap
 
+class Command:
+    ARG_PLACE = "place"
+    ARG_SNAP = "snap"
+
+    def __init__(self, name, args, descr, function):
+        self._name = name
+        self._args = args
+        self._descr = descr
+        self._function = function
+
+    def call(self):
+        return self._function()
+
+    def describe(self):
+        if self._args:
+            argstring = " ".join(map(lambda s: "<" + s + ">",self._args))
+        else:
+            argstring = ""
+        return "  {:<7s} {:<14s} {}".format(self._name,argstring,self._descr)
+
+all = [ ]
+byname = { }
+
+def newCommand(name, args, descr, function):
+    cmd = Command(name, args, descr, function)
+    all.append(cmd)
+    byname[name] = cmd
+    return cmd
+
+def usagemsg():
+    print("Usage: snappy <command> [args]")
+    for cmd in all:
+        print(cmd.describe())
+
+usage = newCommand("help", None, "This usage message", usagemsg)
+
+def check():
+    util.check()
+
+newCommand("check", None, "Perform some configuration checks", check)
+
 def list():
     util.loadSnapshots()
     for name, place in sorted(Place._places.items()):
@@ -15,6 +56,31 @@ def list():
         Snapshot.printHeader()
         for stamp in sorted(snapshots[name].keys(),key = lambda i: int(i)):
             snapshots[name][stamp].printListing()
+
+newCommand("list", None, "List all snapshots", list)
+
+def snapBoth():
+    place = util.getPlace()
+    now = arrow.now().timestamp
+    zfs.createSnapshot(place,now)
+    tarsnap.createSnapshot(place,now)
+
+newCommand("snap", [ Command.ARG_PLACE ], "Create a new snapshot (both ZFS and tarsnap)",
+        snapBoth)
+
+def snapZFS():
+    place = util.getPlace()
+    zfs.createSnapshot(place,arrow.now().timestamp)
+
+newCommand("snapzfs", [ Command.ARG_PLACE ], "Create a new ZFS snapshot",
+        snapZFS)
+
+def snapTS():
+    place = util.getPlace()
+    tarsnap.createSnapshot(place,arrow.now().timestamp)
+
+newCommand("snapts", [ Command.ARG_PLACE ], "Create a new tarsnap snapshot",
+        snapTS)
 
 def nuke():
     util.loadSnapshots()
@@ -25,19 +91,5 @@ def nuke():
     if snap.hasZFS():
         zfs.deleteSnap(snap)
 
-def snapZFS():
-    place = util.getPlace()
-    zfs.createSnapshot(place,arrow.now().timestamp)
+newCommand("nuke", [Command.ARG_PLACE, Command.ARG_SNAP ], "Remove snapsot", nuke)
 
-def snapTS(place = None):
-    place = util.getPlace()
-    tarsnap.createSnapshot(place,arrow.now().timestamp)
-
-def snapBoth():
-    place = util.getPlace()
-    now = arrow.now().timestamp
-    zfs.createSnapshot(place,now)
-    tarsnap.createSnapshot(place,now)
-
-def check():
-    util.check()
